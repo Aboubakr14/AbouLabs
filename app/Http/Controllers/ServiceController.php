162383145f6b2416fd42e02e83806fc87b6ed4f1
Aboubakr14 <adametkawtar@gmail.com>
2020-05-02 14:41:16 +0200

    initialisation du projet et création de la base de donnée

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Service;
+use App\Http\Requests\ServiceRequest;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $services = Service::all();
+
+        return view('admin.servicesIndex',compact('services'));
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.servicesCreate');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ServiceRequest $request)
+    {
+        $service = new Service();
+
+        $service->titre = $request->input('titre');
+        $service->description = $request->input('description');
+        $service->logo = $request->input('logo');
+
+        $service->save();
+
+        return redirect(route('services.index'));
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $service = Service::find($id);
+
+        return view('admin.serviceEdit',compact('service'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ServiceRequest $request, $id)
+    {
+        $service = Service::find($id);
+
+        $service->logo = $request->input('logo');
+        $service->titre = $request->input('titre');
+        $service->description = $request->input('description');
+
+        $service->save();
+
+        return redirect(route('services.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $service = Service::find($id)->delete();
+
+        return redirect(route('services.index'));
+    }
+}
