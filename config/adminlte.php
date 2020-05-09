12bad3b19a5860894713874e3de11ea8c0b84bc9
Aboubakr14 <adametkawtar@gmail.com>
2020-05-09 15:34:40 +0200

    adminlte

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,8 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Labs</b>LTE',
+    'logo_mini' => '<b>Labs</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
@@ -149,23 +150,18 @@ return [
     'right_sidebar_icon' => 'fas fa-cogs',
     'right_sidebar_theme' => 'dark',
     'right_sidebar_slide' => true,
-    'right_sidebar_push' => true,
-    'right_sidebar_scrollbar_theme' => 'os-theme-light',
-    'right_sidebar_scrollbar_auto_hide' => 'l',
 
     /*
     |--------------------------------------------------------------------------
     | URLs
     |--------------------------------------------------------------------------
     |
-    | Here we can modify the url settings of the admin panel.
-    |
-    | For more detailed instructions you can look here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/#69-urls
+    | Register here your dashboard, logout, login and register URLs.
+    | This was automatically set on install, only change if you really need.
+    | logout URL automatically sends a POST request in Laravel 5.3 or higher.
+    | Set register_url to null if you don't want a register link.
     |
     */
-
-    'use_route_url' => false,
 
     'dashboard_url' => 'home',
 
@@ -175,117 +171,304 @@ return [
 
     'register_url' => 'register',
 
-    'password_reset_url' => 'password/reset',
-
-    'password_email_url' => 'password/email',
-
-    'profile_url' => false,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel Mix
-    |--------------------------------------------------------------------------
-    |
-    | Here we can enable the Laravel Mix option for the admin panel.
-    |
-    | For more detailed instructions you can look here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/#610-laravel-mix
-    |
-    */
-
-    'enabled_laravel_mix' => false,
-
     /*
     |--------------------------------------------------------------------------
     | Menu Items
     |--------------------------------------------------------------------------
     |
-    | Here we can modify the sidebar/top navigation of the admin panel.
-    |
-    | For more detailed instructions you can look here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/#611-menu
-    |
+    | Specify your menu items to display in the left sidebar. Each menu item
+    | should have a text and a URL. You can also specify an icon from Font
+    | Awesome. A string instead of an array represents a header in sidebar
+    | layout. The 'can' is a filter on Laravel's built in Gate functionality.
     */
 
-    'menu' => [
+    'menu' =>     
+    [   
+        // ADMIN
         [
-            'text' => 'search',
-            'search' => true,
-            'topnav' => true,
+            'header' => 'User',
+            'can' => 'admin'
         ],
         [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
+            'text' => 'Information profil',
+            'icon' => 'far fa-question-circle',
+            'url' => '/home/info'
         ],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'header' => 'Gestion du site',
+            'can' => 'admin'
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
+            'text' => 'Utilisateur',
+            'icon' => 'fas fa-user',
+            'can' => 'admin',
             'submenu' => [
                 [
-                    'text' => 'level_one',
-                    'url'  => '#',
+                    'text' => 'Ajouter un User',
+                    'icon' => 'fas fa-plus',
+                    'can' => 'admin',
+                    'url' => 'users/create'
                 ],
                 [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
+                    'text' => 'Afficher les users du site',
+                    'icon' => 'fas fa-user',
+                    'url' => 'users',
+                    'can' => 'admin'
+                ]
+            ]
+        ],
+        [
+            'text' => 'Afficher les articles',
+            'icon' => 'fas fa-eye',
+            'can' => 'admin',
+            'url' => 'articles'
+        ],
+        [
+            'text' => 'Validation des articles',
+            'icon' => 'fas fa-check',
+            'can' => 'admin',
+            'url' => '/articles/validation'
+        ],
+        [
+            'header' => 'Page Home',
+            'can' => 'admin',
+        ],
+        [
+            'text' => 'Contenu Textuel',
+            'icon' => 'fas fa-pencil-alt',
+            'url' => '/home/index/edit',
+            'can' => 'admin',
+            
+        ],
+        [
+            'text' => 'Carousel',
+            'icon' => 'far fa-images',
+            'can' => 'admin',
+            'submenu' => [
+                [
+                    'text' => 'Ajout d\'une image',
+                    'icon' => 'fas fa-plus',
+                    'url' => 'medias/create',
                 ],
                 [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
+                    'text' => 'Voir les images',
+                    'icon' => 'far fa-eye',
+                    'url' => 'home/medias'
+                ]
             ],
         ],
-        ['header' => 'labels'],
         [
-            'text'       => 'important',
-            'icon_color' => 'red',
+            'text' => 'Testimonials',
+            'icon' => 'fas fa-user-tag',
+            'can' => 'admin',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter un testimonial',
+                    'icon' => 'fas fa-plus',
+                    'url' => 'testimonials/create'
+                ],
+                [
+                    'text' => 'Voir les testimonials',
+                    'icon' => 'far fa-eye',
+                    'url' => 'home/testimonials'
+                ]
+            ]
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
+            'text' => 'Section Team',
+            'icon' => 'fas fa-users',
+            'can' => 'admin',
+            'submenu' => [
+                [
+                    'text' => 'Ajout un membre de la team',
+                    'icon' => 'fas fa-plus',
+                    'url' => 'teams/create'
+                ],
+                [
+                    'text' => 'Afficher les membres de la team',
+                    'icon' => 'far fa-eye',
+                    'url' => 'home/teams'
+                ]
+            ]
         ],
         [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
+            'text' => 'Section Contact',
+            'icon' => 'fas fa-address-card',
+            'url' => '/home/contact/edit',
+            'can' => 'admin',
+        ],
+        [
+            'text' => 'Bouton Browse',
+            'icon' => 'fas fa-mouse',
+            'url' => 'bouton',
+            'can' => 'admin'
+
+        ],
+        [
+            'header' => 'Page Services',
+            'can' => 'admin',
+        ],
+        [
+            'text' => 'Services',
+            'icon' => 'fas fa-tools',
+            'can' => 'admin',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter un Service',
+                    'icon' => 'fas fa-plus',
+                    'url' => 'services/create',
+                ],
+                [
+                    'text' => 'Afficher les Services',
+                    'icon' => 'far fa-eye',
+                    'url' => '/home/services'
+                ]
+            ]
+        ],
+        [
+            'text' => 'Projet',
+            'icon' => 'fas fa-project-diagram',
+            'can' => 'admin',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter un projet',
+                    'icon' => 'fas fa-plus',
+                    'url' => 'projets/create'
+                ],
+                [
+                    'text' => 'Afficher les projets',
+                    'icon' => 'far fa-eye',
+                    'url' => '/home/projets'
+                ]
+            ]
+           
+        ],
+        //article
+        [
+            'header' => 'Page Blog',
+            'can' => 'admin'
+        ],
+        [
+            'text' => 'Articles',
+            'icon' => 'fas fa-newspaper',
+            'can' => 'admin',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter un article',
+                    'icon' => 'fas fa-plus',
+                    'can' => 'admin',
+                    'url' => 'articles/create'
+                ],
+                [
+                    'text' => 'Afficher les articles',
+                    'icon' => 'fas fa-eye',
+                    'can' => 'admin',
+                    'url' => 'articles'
+                ],
+            ]
+        ],
+        [
+            'text' => 'Categories',
+            'icon' => 'fas fa-th',
+            'can' => 'admin',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter une categorie',
+                    'icon' => 'fas fa-plus',
+                    'can' => 'admin',
+                    'url' => 'categories/create'
+                ],
+                [
+                    'text' => 'Afficher les categories',
+                    'icon' => 'fas fa-eye',
+                    'can' => 'admin',
+                    'url' => 'categories'
+                ],
+            ]
+        ],
+        [
+            'text' => 'Tags',
+            'icon' => 'fas fa-tags',
+            'can' => 'admin',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter un tag',
+                    'icon' => 'fas fa-plus',
+                    'can' => 'admin',
+                    'url' => 'tags/create'
+                ],
+                [
+                    'text' => 'Afficher les tags',
+                    'icon' => 'fas fa-eye',
+                    'can' => 'admin',
+                    'url' => 'tags'
+                ],
+            ]
+        ],
+        
+        
+
+        //Editeur
+        [
+            'header' => 'Articles',
+            'can' => 'editeur',
+        ],
+        [
+            'text' => 'Articles',
+            'icon' => 'fas fa-newspaper',
+            'can' => 'editeur',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter un article',
+                    'icon' => 'fas fa-plus',
+                    'can' => 'editeur',
+                    'url' => 'articles/create'
+                ],
+                [
+                    'text' => 'Afficher les articles',
+                    'icon' => 'fas fa-eye',
+                    'can' => 'editeur',
+                    'url' => 'articles'
+                ],
+            ]
+        ],
+        [
+            'text' => 'Categories',
+            'icon' => 'fas fa-th',
+            'can' => 'editeur',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter une categorie',
+                    'icon' => 'fas fa-plus',
+                    'can' => 'editeur',
+                    'url' => 'categories/create'
+                ],
+                [
+                    'text' => 'Afficher les categories',
+                    'icon' => 'fas fa-eye',
+                    'can' => 'editeur',
+                    'url' => 'categories'
+                ],
+            ]
+        ],
+        [
+            'text' => 'Tags',
+            'icon' => 'fas fa-tags',
+            'can' => 'editeur',
+            'submenu' => [
+                [
+                    'text' => 'Ajouter un tag',
+                    'icon' => 'fas fa-plus',
+                    'can' => 'editeur',
+                    'url' => 'tags/create'
+                ],
+                [
+                    'text' => 'Afficher les tags',
+                    'icon' => 'fas fa-eye',
+                    'can' => 'editeur',
+                    'url' => 'tags'
+                ],
+            ]
         ],
     ],
 
@@ -294,10 +477,10 @@ return [
     | Menu Filters
     |--------------------------------------------------------------------------
     |
-    | Here we can modify the menu filters of the admin panel.
-    |
-    | For more detailed instructions you can look here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/#612-menu-filters
+    | Choose what filters you want to include for rendering the menu.
+    | You can add your own filters to this array after you've created them.
+    | You can comment out the GateFilter if you don't want to use Laravel's
+    | built in Gate functionality
     |
     */
 
@@ -309,46 +492,42 @@ return [
         JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
-        JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class,
-    ],
+    ],  
 
     /*
     |--------------------------------------------------------------------------
     | Plugins Initialization
     |--------------------------------------------------------------------------
     |
-    | Here we can modify the plugins used inside the admin panel.
-    |
-    | For more detailed instructions you can look here:
-    | https://github.com/jeroennoten/Laravel-AdminLTE/#613-plugins
+    | Configure which JavaScript plugins should be included. At this moment,
+    | DataTables, Select2, Chartjs and SweetAlert are added out-of-the-box,
+    | including the Javascript and CSS files from a CDN via script and link tag.
+    | Plugin Name, active status and files array (even empty) are required.
+    | Files, when added, need to have type (js or css), asset (true or false) and location (string).
+    | When asset is set to true, the location will be output using asset() function.
     |
     */
 
     'plugins' => [
         [
             'name' => 'Datatables',
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
                     'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
-                ],
-                [
-                    'type' => 'js',
-                    'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
+                    'location' => '//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js',
                 ],
                 [
                     'type' => 'css',
                     'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
+                    'location' => '//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.css',
                 ],
             ],
         ],
         [
             'name' => 'Select2',
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
@@ -364,7 +543,7 @@ return [
         ],
         [
             'name' => 'Chartjs',
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
@@ -375,7 +554,7 @@ return [
         ],
         [
             'name' => 'Sweetalert2',
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
@@ -386,7 +565,7 @@ return [
         ],
         [
             'name' => 'Pace',
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'css',
