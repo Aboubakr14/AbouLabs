2afbd3c3827641da4a85ef1c5390cb363cc276f0
Aboubakr14 <adametkawtar@gmail.com>
2020-05-11 14:13:30 +0200

    mise en lien du backoffice avec les routes

---
+++ resources/views/admin/bouton.blade.php
@@ -0,0 +1,32 @@
+@extends('adminlte::page')
+
+@extends('templates.index')
+
+@include('templates.head')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Page Home</h1>
+@stop
+
+@section('content')
+<div class="container">
+    <form action="{{route('bouton.update',1)}}" method="POST">
+        @csrf
+        @method('put')
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </div>
+        @endif
+        <div class="form-group">
+            <label for="texte_carousel">texte du boutton browse</label>
+            <input type="text" class="form-control" name="texte" id="texte_carousel" value="{{ isset($bouton) != null ? $bouton->texte : ''}}">
+        </div>
+        <button class="btn btn-primary mx-auto">Enregistrer</button>
+    </form>
+</div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,19 +10,79 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Modem
+use App\User;
+use App\Projet;
+use App\Article;
+use App\Tag;
+use App\ArticleTag;
+use App\Commentaire;
+use App\Service;
+use App\Categorie;
+use App\Bouton;
+use App\Newsletter;
+//Mail
+use App\Mail\ArticleValidation;
+use App\Mail\ArticleNew;
+use App\Mail\SendMessage;
+use App\Mail\WelcomeNewsletter;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\MessageRequest;
+use App\Http\Requests\NewsletterRequest;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\Paginator;
 
-Route::get('/', function () {
-    return view('templates/index');
+//View Labs
+Route::get('/','HomeController@index')->name('index');
+
+Route::get('/service', function(){
+    //Je fais passer le nom pour pourvoir dynamiser chemin.blade
+    // qu'il afficher le nom de la page acutel
+    $name = "Services";
+    $actif = "services";
+    $services = DB::table('services')->paginate(9);
+    $projets = Projet::all();
+    $bouton = Bouton::find(1);
+    $smartphone = Service::select('*')->orderBy('id','desc')->get();
+
+    if(count($projets)>=3)
+        $projets = $projets->random(3);
+
+    return view('service',compact('name','actif','services','projets','bouton','smartphone'));
 });
 
-Auth::routes();
+Route::get('/blog', function(){
+    $name = "Blog";
+    $actif = "blog";
 
-Route::get('/home', 'HomeController@index')->name('home');
+    $articles = Article::where('etat','=','Publié')->orderBy('id','desc')->paginate(3);
+    $tags = Tag::all();
+    $articleTags = ArticleTag::all();
+    $users = User::all();
+    $commentaires = Commentaire::all();
+    $categories = Categorie::all();
 
-Auth::routes();
+    return view('blog',compact('name','actif','articles','tags','users','articleTags','commentaires','categories'));
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/blog-post/{id}', function(){
+    $name = "Blog";
+    $actif = "blog";
 
-Auth::routes();
+    $article = Article::find(request('id'));
+    $tags = Tag::all();
+    $articleTags = ArticleTag::all();
+    $users = User::all();
+    $categories = Categorie::all();
+    $commentaires = Commentaire::where('id_article','=',$article->id)
+        ->orderBy('id','desc')
+        ->paginate(5);  
+    
 
-Route::get('/home', 'HomeController@index')->name('home');
+    return view('blogPost',compact('name','actif','article','tags','users','articleTags','commentaires','categories'));
+});
+
+Route::resource('commentaires','CommentaireController');
+Route::post('/blog-post/{id}/commentaire','CommentaireController@store');
+Route::delete('/blog-post/{id}/commentaire/delete','CommentaireController@destroy');
+
